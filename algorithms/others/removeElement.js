b54f3b1b12edf3bb35c0a95c6c9c7e8472e36576
BlackCcccCcc <827187153@qq.com>
2023-09-03 23:09:26 +0800

    feat: 移除元素(双指针优化)

--- algorithms/others/removeElement.js
+++ algorithms/others/removeElement.js
@@ -1,11 +1,23 @@
 const removeElemnt = (nums, val) => {
-    const len = nums.length;
-    let left = 0;
-    for (let right = 0; right < len; right++) {
-        console.log('left', nums[left]);
-        console.log('right', nums[right]);
-        if (nums[right] !== val) {
-            nums[left] = nums[right];
+    // const len = nums.length;
+    // let left = 0;
+    // for (let right = 0; right < len; right++) {
+    //     console.log('left', nums[left]);
+    //     console.log('right', nums[right]);
+    //     if (nums[right] !== val) {
+    //         nums[left] = nums[right];
+    //         left++;
+    //     }
+    // }
+    // return left;
+
+    let left = 0,
+        right = nums.length;
+    while (left < right) {
+        if (nums[left] === val) {
+            nums[left] = nums[right - 1];
+            right--;
+        } else {
             left++;
         }
     }
