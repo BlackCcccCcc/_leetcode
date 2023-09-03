c4ab91d5f53b7a99bb6a569911a1a1d11df1896f
BlackCcccCcc <827187153@qq.com>
2023-09-03 23:33:51 +0800

    feat: 删除元素

--- algorithms/others/removeElement.js
+++ algorithms/others/removeElement.js
@@ -1,27 +1,26 @@
 const removeElemnt = (nums, val) => {
-    // const len = nums.length;
-    // let left = 0;
-    // for (let right = 0; right < len; right++) {
-    //     console.log('left', nums[left]);
-    //     console.log('right', nums[right]);
-    //     if (nums[right] !== val) {
-    //         nums[left] = nums[right];
-    //         left++;
-    //     }
-    // }
-    // return left;
-
-    let left = 0,
-        right = nums.length;
-    while (left < right) {
-        if (nums[left] === val) {
-            nums[left] = nums[right - 1];
-            right--;
-        } else {
+    const len = nums.length;
+    let left = 0;
+    for (let right = 0; right < len; right++) {
+        console.log('left', nums[left]);
+        console.log('right', nums[right]);
+        if (nums[right] !== val) {
+            nums[left] = nums[right];
             left++;
         }
     }
     return left;
+    // let left = 0,
+    //     right = nums.length;
+    // while (left < right) {
+    //     if (nums[left] === val) {
+    //         nums[left] = nums[right - 1];
+    //         right--;
+    //     } else {
+    //         left++;
+    //     }
+    // }
+    // return left;
 };
 
 const nums = [0, 1, 3, 2, 4, 5, 6, 2, 4];
