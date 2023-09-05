0237d87070f5f4879b6c793badb666a83f834448
BlackCcccCcc <827187153@qq.com>
2023-09-05 21:33:14 +0800

    fix: todo KMP算法

--- algorithms/others/strStr.js
+++ algorithms/others/strStr.js
@@ -1,24 +1,27 @@
 // BF
-const strStr = (haystack, needle) => {
-    const n = haystack.length;
-    const m = needle.length;
-    // 不能超过haystack的长度
-    for (let i = 0; i + m < n; i++) {
-        let flag = true;
-        for (let j = 0; j < m; j++) {
-            if (haystack[i + j] !== needle[j]) {
-                flag = false;
-                break;
-            }
-        }
+// const strStr = (haystack, needle) => {
+//     const n = haystack.length;
+//     const m = needle.length;
+//     // 不能超过haystack的长度
+//     for (let i = 0; i + m < n; i++) {
+//         let flag = true;
+//         for (let j = 0; j < m; j++) {
+//             if (haystack[i + j] !== needle[j]) {
+//                 flag = false;
+//                 break;
+//             }
+//         }
 
-        if (flag) {
-            return i;
-        }
-    }
+//         if (flag) {
+//             return i;
+//         }
+//     }
 
-    return -1;
-};
+//     return -1;
+// };
+
+// KMP
+const strStr = (haystack, needle) => {};
 let haystack = 'ssadbutsad';
 let needle = 'sad';
 console.log(strStr(haystack, needle));
