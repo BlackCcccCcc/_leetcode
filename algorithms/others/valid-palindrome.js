f16dfd3e664637e7dedfd6ca7dbaeadbdd6e5a36
BlackCcccCcc <827187153@qq.com>
2023-10-25 23:29:40 +0800

    fix: 优化

--- algorithms/others/valid-palindrome.js
+++ algorithms/others/valid-palindrome.js
@@ -1,7 +1,16 @@
 var isPalindrome = function (s) {
-    s = s
-        .replace(/[^a-zA-Z0-9]/g, '')
-        .replace(/\s/g, '')
-        .toLowerCase();
-    return s === [...s].reverse().join('');
+    // s = s
+    //     .replace(/[^a-zA-Z0-9]/g, '')
+    //     .replace(/\s/g, '')
+    //     .toLowerCase();
+    // return s === [...s].reverse().join('');
+
+    s = s.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+
+    let [left, right] = [0, s.length - 1];
+    while (left < right) {
+        if (s[left++] !== s[right--]) return false;
+    }
+
+    return true;
 };
