a8abb0eb8c6895cc7b0be3a1be6f04f785f91351
BlackCcccCcc <827187153@qq.com>
2023-10-27 21:41:39 +0800

    feat: 优化

---
+++ algorithms/others/balanced-binary-tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+    const getDepth = (node) => {
+        if (node === null) {
+            return 0;
+        }
+        let leftDepth = getDepth(node.left);
+        if (leftDepth === -1) {
+            return -1;
+        }
+        let rightDepth = getDepth(node.right);
+        if (rightDepth === -1) {
+            return -1;
+        }
+        if (Math.abs(rightDepth - leftDepth) > 1) {
+            return -1;
+        } else {
+            return 1 + Math.max(rightDepth, leftDepth);
+        }
+    };
+
+    return !(getDepth(root) === -1);
+};
+
+const test = [1, 2, 2, 3, 3, null, null, 4, 4];
